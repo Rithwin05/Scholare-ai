81ad37505f7771949178482de259fe9063f7c133
Rithwin05 <109716807+Rithwin05@users.noreply.github.com>
2024-02-22 14:16:58 +0530

    database update

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,25 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'; // Add if using dotenv
+
+dotenv.config(); // Load environment variables (if using dotenv)
 
 declare global {
   var prisma: PrismaClient | undefined;
-};
+}
 
-const prismadb = globalThis.prisma || new PrismaClient()
+const prismadb = globalThis.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: env("DATABASE_URL"),
+    },
+  },
+});
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
+
+function env(key: string) {
+  return process.env[key];
+}
+
